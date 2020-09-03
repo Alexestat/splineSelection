3bbef69e3d8fdd62184ba5dd663a461d4b32cb0d
Rodrigo Carvalho <rodrigorsdc@gmail.com>
2020-09-03 14:27:34 -0300

    add cpp file

---
+++ not_able_knots.cpp
@@ -0,0 +1,19 @@
+#include <Rcpp.h>
+#include <list>
+
+// [[Rcpp::export]]
+Rcpp::NumericVector
+not_able_knots(Rcpp::NumericMatrix all, int delta) {
+    std::list<int> notAble;
+    int k = 0;	
+    for(int i = 0; i < all.ncol(); i++) {
+	for (int j = 0; j < all.nrow() - 1; j++) {
+	    if (all(j + 1, i) - all(j, i) <= delta) {
+		notAble.push_back(i+1);
+		break;
+	    }
+	}
+    }
+    Rcpp::NumericVector RnotAble = Rcpp::wrap(notAble);
+    return RnotAble;
+}
